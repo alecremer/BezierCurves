df7ba1e4437b5bfb0221c229b3800c34fedfb45f
Ale192837 <alexandre.2018@alunos.utfpr.edu.br>
2023-01-21 18:58:27 -0300

    Add readme, chouse keeping

--- ACBezierCurve/BezierCurve.cs
+++ ACBezierCurve/BezierCurve.cs
@@ -1,41 +1,122 @@
-﻿using System.Collections;
+﻿using System.Numerics;
+using System.Security.AccessControl;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Vector3 = UnityEngine.Vector3;
 
-namespace ACBezier
+namespace BezierCurves
 {
+    public class BakedPoint
+    {
+
+        public Vector3 point; 
+        public Vector3 tangent;
+        public Vector3 normal;
+
+    }
+    
     [ExecuteInEditMode]
     public class BezierCurve : MonoBehaviour
     {
 
-        public int numberOfPointsInGizmos;
-        public float giszmosRadius;
-        public bool reloading = true;
-        public Color color;
-        public List<GameObject> points = new List<GameObject>();
+        [System.Serializable]
+        public class ControlPoint
+        {
+            public GameObject point;
+            public Color color;
+        
+        }
 
+        
+
+        public List<ControlPoint> controlPoints;
+
+        public int curveDebuggerResolution;
+        public float pointsDebugRadius = 1f;
+        public bool reloading = true;
+        public Color lineColor = Color.red;
+        public Color tangentColor = Color.yellow;
+        public float tangentLenght = 1;
+        public Color normalColor = Color.blue;
+        public float normalLenght = 1;
+        public Color viewPointColor = Color.green;
+        public float viewPointT = 0;
+        public float viewPointRadius = 0;
+
+        // Called when scene visual debugger are activated
         private void OnDrawGizmos()
         {
-            Gizmos.color = color;
+            Gizmos.color = lineColor;
+
+
+
             if (reloading)
             {
-                for (int i = 1; i <= numberOfPointsInGizmos; i++)
+                
+                Vector3 lastPoint = Vector3.zero;
+                Vector3 lastTangent = Vector3.zero;
+
+                for (int i = 1; i <= curveDebuggerResolution; i++)
                 {
-                    Gizmos.DrawLine(Curve(0, (float)(i - 1) / numberOfPointsInGizmos), Curve(0, (float)i / numberOfPointsInGizmos));
+                    
+                    Gizmos.color = lineColor;
+                
+                    lastPoint = Curve((float)(i - 1) / curveDebuggerResolution);
+                    Vector3 currentPoint = Curve((float)i / curveDebuggerResolution);
+
+                    Gizmos.DrawLine(lastPoint, currentPoint);
+
+
+                    // Draw tangent
+
+                    Gizmos.color = tangentColor;
+
+                    Vector3 tangentVector = (currentPoint - lastPoint).normalized;
+
+                    Gizmos.DrawLine(currentPoint, currentPoint + tangentVector * tangentLenght);
+
+                    
+                    
+                    // Draw normal 
+
+                    Gizmos.color = normalColor;
+
+                    Vector3 normalVector = (tangentVector - lastTangent).normalized;
+
+                    Gizmos.DrawLine(currentPoint, currentPoint + normalVector.normalized * normalLenght);
+
+                    lastTangent = tangentVector;
+                
                 }
+
+                Gizmos.color = viewPointColor;
+                Gizmos.DrawSphere(Curve(viewPointT), viewPointRadius);
             }
+            
+            for(int i=0; i<controlPoints.Count; i++)
+            {
+
+                try
+                {
+                    Gizmos.color = controlPoints[i].color;
+                    Gizmos.DrawSphere(controlPoints[i].point.transform.position, pointsDebugRadius);
+                }
+                catch{}
+            
+
+            } 
         }
 
-        void Start()
-        {
 
-        }
-
-        float CoefficientsObj(int pointIndex)
+        float PolynomialCoefficient(int pointIndex, List<ControlPoint> controlPointsCurrent)
         {
-            float a = Factorial(points.Count - 1) / (Factorial(pointIndex) * Factorial(points.Count - pointIndex - 1));
+            float a = Factorial(controlPointsCurrent.Count - 1) / (Factorial(pointIndex) * Factorial(controlPointsCurrent.Count - pointIndex - 1));
             return a;
         }
+
+
         float Factorial(float n)
         {
             try
@@ -47,22 +128,70 @@ namespace ACBezier
             catch { Debug.LogError("n must be greater than -1"); return 1; }
         }
 
-        Vector3 Curve(int i, float t)
+
+        public Vector3 Curve(float curveStep, int controlPointIndex = 0)
         {
             Vector3 a;
-            if (i < points.Count)
+
+
+
+            if (controlPointIndex < controlPoints.Count)
             {
-                a = CoefficientsObj(i) * Mathf.Pow((1f - t), (points.Count - i - 1)) * Mathf.Pow(t, i) * points[i].transform.position + Curve(i + 1, t);
+
+                // A = (Sum from 0 to points.count) Coefficient * (1 - curveStep) ** (points.count -i -1) * point
+                a = PolynomialCoefficient(controlPointIndex, controlPoints) * Mathf.Pow((1f - curveStep), (controlPoints.Count - controlPointIndex - 1)) * Mathf.Pow(curveStep, controlPointIndex) * controlPoints[controlPointIndex].point.transform.position + Curve(curveStep, controlPointIndex + 1);
 
             }
+
             else a = Vector3.zero;
             return a;
         }
 
-        void Update()
+
+        public List<BakedPoint> BakeCurve(float curveStepLenght)
         {
 
+            List<Vector3> curveBaked = new List<Vector3>();
+            List<BakedPoint> bakedCurve = new List<BakedPoint>();
+
+            Vector3 currentPoint;
+            Vector3 lastPoint = Vector3.zero;
+            Vector3 lastTangent = Vector3.zero;
+
+
+            for(float curveStep = 0; curveStep < controlPoints.Count; curveStep += curveStepLenght)
+            {
+
+                BakedPoint bakedPoint = new BakedPoint();
+                currentPoint = Curve(curveStep/controlPoints.Count);
+                curveBaked.Add(currentPoint);
+
+                bakedPoint.point = currentPoint;
+
+                // tangent
+
+                Vector3 tangentVector = (currentPoint - lastPoint).normalized;
+
+                bakedPoint.tangent = tangentVector;
+
+                // normal 
+
+                Vector3 normalVector = (tangentVector - lastTangent).normalized;
+
+                bakedPoint.normal = normalVector;
+
+                bakedCurve.Add(bakedPoint);
+
+                lastPoint = currentPoint;
+                lastTangent = tangentVector;
+
+            }
+
+
+            return bakedCurve;
+
         }
+
     }
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# API that creates bezier curves based on N control points. It's an old version that have a new unpublished update.
+# Unity Bezier curves
+API that creates Bezier curves based on N control points.
+
+- Edit mode debugger;
+
+
+# Methods
+
+## Curve
+public Vector3 Curve(float curveStep, int controlPointIndex = 0)
+
+Calculate Bezier curve using control points
+
+##### Arguments
+curveStep: parameter of parametric curve that walks along the curve. 0 is the start of curve, 1 is the end of curve;
+controlPointIndex: index of control point in sum of all control points. For calculate whole curve, don't use this parameter.
+
+
+##### Return
+Point in space
+
+
+## BakeCurve
+public List<BakedPoint> BakeCurve(float curveStepLenght)
+
+Calculate curve and store in list of baked point. It saves processing in game time
+
+##### Arguments
+curveStepLenght: parameter of parametric curve that walks along the curve. 0 is the start of curve, 1 is the end of curve;
+
+
+##### Return
+List of BakedPoint
+
+# Classes
+
+## BakedPoint
+Precalculated curve point
+
+##### Properties
+
+public Vector3 point: world point
+public Vector3 tangent: point tangent vector
+public Vector3 normal: point normal vector
+
+
+## ControlPoin
+Control point of Bezier curve
+
+##### Properties
+
+public GameObject point: object to get transform
+public Color color: color of debug point
